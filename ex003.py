3c7dbbbd8a78b1ce33a7141cc92a02eb5351aa00
Marcell Lévai <marcell-levai@outlook.com>
2018-05-05 19:57:29 +0200

    Third excercise finished

---
+++ ex003.py
@@ -0,0 +1,55 @@
+# Excercise
+
+print("I will now count my chickens:")
+# * and / preceeds %, % preceeds + and -
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# * and / preceeds %
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# Study Drills
+
+print("I will now count my chickens:")
+# * and / preceeds %, % preceeds + and -
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+# * and / preceeds %
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
+
+# PEMDAS: Parentheses Exponents Multiplication Division Addition Subtraction
+# PE(M&D)(A&S)
